84fdc49522a59709e4364d4279037a2d32513509
WandirPereira <wandir@gmail.com>
2022-01-24 11:17:21 -0300

    Criando CRUD

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,14 +1,97 @@
+import res from 'express/lib/response';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
+    }
+  }
+
+  // Index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // Index
+  async show(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          erros: ['ID não enviado!'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          erros: ['Usuário não existe!'],
+        });
+      }
+
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // Index
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!req.params.id) {
+        return res.status(400).json({
+          erros: ['ID não enviado!'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          erros: ['Usuário não existe!'],
+        });
+      }
+
+      const novosdados = await user.update(req.body);
+      return res.json(novosdados);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!req.params.id) {
+        return res.status(400).json({
+          erros: ['ID não enviado!'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          erros: ['Usuário não existe!'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(null);
+    } catch (e) {
+      return res.json(null);
     }
   }
 }
