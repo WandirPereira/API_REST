3435c257f8f804e38f5b42180ed364a543272afc
WandirPereira <wandir@gmail.com>
2022-01-24 17:21:37 -0300

    Implementado authenticação com JWT

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -16,7 +16,8 @@ class UserController {
   // Index
   async index(req, res) {
     try {
-      const users = await User.findAll();
+      // const users = await User.findAll();
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
       return res.json(users);
     } catch (e) {
       return res.json(null);
@@ -40,23 +41,19 @@ class UserController {
         });
       }
 
-      return res.json(user);
+      const { id, nome, email } = user;
+
+      // return res.json(user);
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.json(null);
     }
   }
 
-  // Index
+  // Update
   async update(req, res) {
     try {
-      const { id } = req.params;
-      if (!req.params.id) {
-        return res.status(400).json({
-          erros: ['ID não enviado!'],
-        });
-      }
-
-      const user = await User.findByPk(id);
+      const user = await User.findByPk(req.userId);
 
       if (!user) {
         return res.status(400).json({
@@ -73,14 +70,14 @@ class UserController {
 
   async delete(req, res) {
     try {
-      const { id } = req.params;
-      if (!req.params.id) {
-        return res.status(400).json({
-          erros: ['ID não enviado!'],
-        });
-      }
+      // const { id } = req.params;
+      // if (!req.params.id) {
+      //   return res.status(400).json({
+      //     erros: ['ID não enviado!'],
+      //   });
+      // }
 
-      const user = await User.findByPk(id);
+      const user = await User.findByPk(req.userId);
 
       if (!user) {
         return res.status(400).json({
--- src/models/User.js
+++ src/models/User.js
@@ -49,4 +49,8 @@ export default class Users extends Model {
 
     return this;
   }
+
+  passwordIsValid(password) {
+    return bcryptjs.compare(password, this.password_hash);
+  }
 }
